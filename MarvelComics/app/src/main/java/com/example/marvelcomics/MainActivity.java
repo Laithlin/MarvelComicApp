20f0502b8444e5e56f9fc82e29b9dac3371b4445
Laithlin <justynasikorska15@gmail.com>
2022-06-21 22:15:28 +0200

    read data

--- MarvelComics/app/src/main/java/com/example/marvelcomics/MainActivity.java
+++ MarvelComics/app/src/main/java/com/example/marvelcomics/MainActivity.java
@@ -1,28 +1,45 @@
 package com.example.marvelcomics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.net.URL;
 import java.util.List;
 import java.util.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<ArrayList<String>> c_data = new ArrayList<ArrayList<String>>();
+//    private final ArrayList<String[]> c_data = new ArrayList<String[]>();
+    private String[] titles_array = new String[25];
+    private String[] desc_array = new String[25];
+    private String[] img_array = new String[25];
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recView);
         Log.e("error", " dziala");
 
         getJsonData();
+        Log.e("error", this.titles_array[0]);
+        MyAdapter myAdapter = new MyAdapter(this, this.titles_array, this.desc_array, this.img_array);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
     private void getJsonData(){
@@ -35,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 //            ArrayList<ArrayList<String>> c_data = new ArrayList<ArrayList<String>>();
             for (int i = 0; i < comics.length(); i ++){
                 JSONObject temp = comics.getJSONObject(i);
-                ArrayList<String> temp_list = new ArrayList<String>();
+//                ArrayList<String> temp_list = new ArrayList<String>();
 
                 String temp_title = temp.getString("title");
                 Log.d("Details-->", temp_title);
@@ -46,13 +63,16 @@ public class MainActivity extends AppCompatActivity {
 //                String image = img_temp.getString("path");
                 String image = checkImage(img);
                 Log.d("Image", image);
-                temp_list.add(temp_title);
-                temp_list.add(temp_description);
-                temp_list.add(image);
-
-                this.c_data.add(temp_list);
-                ArrayList<String> m = c_data.get(0);
-                Log.d("Results", m.get(0));
+                String[] temp_list = {temp_title, temp_description, image};
+//                temp_list.add(temp_title);
+//                temp_list.add(temp_description);
+//                temp_list.add(image);
+                this.titles_array[i] = temp_title;
+                this.desc_array[i] = temp_description;
+                this.img_array[i] = image;
+//                this.c_data.add(temp_list);
+//                String[] m = c_data.get(0);
+//                Log.d("Results", m[0]);
 
             }
         } catch (Exception e) {
@@ -67,5 +87,15 @@ public class MainActivity extends AppCompatActivity {
             return "empty";
         }
 
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
---
+++ MarvelComics/app/src/main/java/com/example/marvelcomics/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.example.marvelcomics;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String titles[], descrptions[];
+    String[] imgs;
+    Context context;
+
+    public MyAdapter (Context ct, String title[], String description[], String images[]){
+        context = ct;
+        titles = title;
+        descrptions = description;
+        imgs = images;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.titleView.setText(titles[position]);
+        holder.descView.setText(descrptions[position]);
+//        holder.coverView.setImageResource(imgs[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 25;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titleView, descView;
+        ImageView coverView;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleView = itemView.findViewById(R.id.comic_title);
+            descView = itemView.findViewById(R.id.comic_description);
+            coverView = itemView.findViewById(R.id.comic_cover);
+        }
+    }
+}
