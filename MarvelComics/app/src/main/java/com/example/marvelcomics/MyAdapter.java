394cbbba4062fdd125b3fa8291960d69c2970cd0
Laithlin <justynasikorska15@gmail.com>
2022-06-22 15:57:16 +0200

    no search result - can be better

--- MarvelComics/app/src/main/java/com/example/marvelcomics/MyAdapter.java
+++ MarvelComics/app/src/main/java/com/example/marvelcomics/MyAdapter.java
@@ -2,6 +2,7 @@ package com.example.marvelcomics;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     ArrayList<String> titles, descrptions;
     ArrayList<String> imgs;
     Context context;
+    String searchResult = "";
 
     public MyAdapter (Context ct, ArrayList<String> title, ArrayList<String> description, ArrayList<String> images){
         context = ct;
@@ -33,9 +35,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         descrptions = new ArrayList<String>();
         imgs = new ArrayList<String>();
 
-        titles = title;
-        descrptions = desc;
-        imgs = img;
+        Log.e("wielkosc", String.valueOf(title.size()));
+
+        if (title.size() == 0){
+            searchResult = "There's no such comic....";
+            titles = title;
+            descrptions = desc;
+            imgs = img;
+        }
+        else{
+            titles = title;
+            descrptions = desc;
+            imgs = img;
+            searchResult = "";
+        }
 
         notifyDataSetChanged();
     }
@@ -52,6 +65,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.titleView.setText(titles.get(position));
         holder.descView.setText(descrptions.get(position));
+        holder.searchView.setText(searchResult);
         holder.coverView.setImageDrawable(Drawable.createFromPath(imgs.get(position)));
     }
 
@@ -62,7 +76,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView titleView, descView;
+        TextView titleView, descView, searchView;
         ImageView coverView;
 
 
@@ -71,6 +85,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             titleView = itemView.findViewById(R.id.comic_title);
             descView = itemView.findViewById(R.id.comic_description);
             coverView = itemView.findViewById(R.id.comic_cover);
+            searchView = itemView.findViewById(R.id.search);
         }
 
         public void getFilter() {
